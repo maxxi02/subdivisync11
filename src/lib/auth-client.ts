ead037353bcc03700862400e0d4a5b10271b6f50
maxxi <129664117+maxxi02@users.noreply.github.com>
2025-08-31 16:00:38 +0800

    fxied origin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient, twoFactorClient } from "better-auth/client/plugins";
 import { admin, ac, tenant } from "@/better-auth/permissions";
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
   baseURL:
     process.env.NODE_ENV === "production"
       ? (process.env.NEXT_PUBLIC_URL as string)
